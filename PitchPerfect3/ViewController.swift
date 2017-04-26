4435dc9b438a879652775b984ba29cfc8a65dae1
Bryan's Air <BryansAppleAir@BryBook.local>
2017-04-25 20:07:41 -0600

    asset images added, recording and stop botton images initaialized

--- PitchPerfect3/ViewController.swift
+++ PitchPerfect3/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,12 +34,10 @@ class ViewController: UIViewController {
         recordingLabel.text = "Recording in Process"
     }
 
-    @IBOutlet weak var recordButton: UIButton!
     
     @IBAction func stopRecording(_ sender: Any) {
     print("Stop Recording Button Pressed")
     }
     
-    @IBOutlet weak var stopRecordingButton: UIButton!
 }
 
